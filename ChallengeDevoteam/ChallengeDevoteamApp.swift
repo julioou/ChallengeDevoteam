1591e7283793788f9948066264055ac4743de25e
Júlio Assis <you@example.com>
2024-03-19 09:06:32 +0000

    Initial Commit

---
+++ ChallengeDevoteam/ChallengeDevoteamApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeDevoteamApp.swift
+//  ChallengeDevoteam
+//
+//  Created by Julio Cesar on 19/03/24.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeDevoteamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
